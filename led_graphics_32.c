03c5c9fd78e502db4577db26670c098d20e5168b
asiddiqua004 <48198420+asiddiqua004@users.noreply.github.com>
2019-11-12 17:36:51 -0800

    Add files via upload

---
+++ led_graphics_32.c
@@ -0,0 +1,271 @@
+#include "led_graphics_32.h"
+#include "led_matrix_32.h"
+
+void displayGridBorders(Color color) {
+  // change data accordingly for 64 bit
+  // The grid consists of 2 lines
+  // data_size columns_borders = 0xC000000000000003;
+  data_size columns_borders = 0xC000000000000003;
+  data_size row_borders = 0xFFFFFFFFFFFFFFFF;
+  setRowData(0, color, row_borders);
+  setRowData(1, color, row_borders);
+  setRowData((matrix_width - 1), color, row_borders);
+  setRowData((matrix_width - 2), color, row_borders);
+  for (uint32_t i = 2; i < matrix_width - 2; i++) {
+    setRowData(i, color, columns_borders);
+  }
+}
+
+/**************Setting  Lines for Pacman- Begins*****************/
+void set_line1_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(row, column, packman_color);
+    setPixel(row, column - 1, packman_color);
+    setPixel(row, column + 1, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row, column, packman_color);
+    setPixel(row - 1, column, packman_color);
+    setPixel(row + 1, column, packman_color);
+  }
+}
+void set_line2_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 1 : row - 1, column + 2, packman_color);
+    setPixel(var_left ? row + 1 : row - 1, column + 1, packman_color);
+    setPixel(var_left ? row + 1 : row - 1, column + 0, packman_color);
+    setPixel(var_left ? row + 1 : row - 1, column - 1, packman_color);
+    setPixel(var_left ? row + 1 : row - 1, column - 2, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 2, var_down ? column - 1 : column + 1, packman_color);
+    setPixel(row + 1, var_down ? column - 1 : column + 1, packman_color);
+    setPixel(row + 0, var_down ? column - 1 : column + 1, packman_color);
+    setPixel(row - 1, var_down ? column - 1 : column + 1, packman_color);
+    setPixel(row - 2, var_down ? column - 1 : column + 1, packman_color);
+  }
+}
+void set_line3_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 2 : row - 2, column + 3, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 2, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 1, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 0, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column - 1, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column - 2, packman_color);
+    setPixel(var_left ? row + 2 : row - 2, column - 3, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 3, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row + 2, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row + 1, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row + 0, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row - 1, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row - 2, var_down ? column - 2 : column + 2, packman_color);
+    setPixel(row - 3, var_down ? column - 2 : column + 2, packman_color);
+  }
+}
+void set_line4_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 3 : row - 3, column - 3, packman_color);
+    setPixel(var_left ? row + 3 : row - 3, column - 2, packman_color);
+    setPixel(var_left ? row + 3 : row - 3, column - 1, packman_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 1, packman_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 2, packman_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 3, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 3, var_down ? column - 3 : column + 3, packman_color);
+    setPixel(row + 2, var_down ? column - 3 : column + 3, packman_color);
+    setPixel(row + 1, var_down ? column - 3 : column + 3, packman_color);
+    setPixel(row - 1, var_down ? column - 3 : column + 3, packman_color);
+    setPixel(row - 2, var_down ? column - 3 : column + 3, packman_color);
+    setPixel(row - 3, var_down ? column - 3 : column + 3, packman_color);
+  }
+}
+void set_line5_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 4 : row - 4, column + 3, packman_color);
+    setPixel(var_left ? row + 4 : row - 4, column + 2, packman_color);
+    setPixel(var_left ? row + 4 : row - 4, column - 2, packman_color);
+    setPixel(var_left ? row + 4 : row - 4, column - 3, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 3, var_down ? column - 4 : column + 4, packman_color);
+    setPixel(row + 2, var_down ? column - 4 : column + 4, packman_color);
+    setPixel(row - 2, var_down ? column - 4 : column + 4, packman_color);
+    setPixel(row - 3, var_down ? column - 4 : column + 4, packman_color);
+  }
+}
+void set_line6_pac(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color packman_color = Lime;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 5 : row - 5, column + 3, packman_color);
+    setPixel(var_left ? row + 5 : row - 5, column - 3, packman_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 3, var_down ? column - 5 : column + 5, packman_color);
+    setPixel(row - 3, var_down ? column - 5 : column + 5, packman_color);
+  }
+}
+
+/**************Setting Lines for Pacman - Ends*****************/
+
+void drawPackMan(int row, int column, direction_e direction) {
+  // fillFrameBuffer_Inside_Grid_Black();
+  bool var_up = 0, var_down = 0, var_left = 0, var_right = 0;
+
+  if (direction == LEFT_UP || direction == LEFT_DOWN) {
+    var_left = 1;
+  }
+  if (direction == UP_RIGHT || direction == UP_LEFT) {
+    var_up = 1;
+  }
+  if (direction == RIGHT_DOWN || direction == RIGHT_UP) {
+    var_right = 1;
+  }
+  if (direction == DOWN_LEFT || direction == DOWN_RIGHT) {
+    var_down = 1;
+  }
+  set_line1_pac(row, column, var_up, var_down, var_left, var_right);
+  set_line2_pac(row, column, var_up, var_down, var_left, var_right);
+  set_line3_pac(row, column, var_up, var_down, var_left, var_right);
+  set_line4_pac(row, column, var_up, var_down, var_left, var_right);
+  set_line5_pac(row, column, var_up, var_down, var_left, var_right);
+  set_line6_pac(row, column, var_up, var_down, var_left, var_right);
+}
+
+/**************Setting  Lines for Ball- Begins*****************/
+void set_line1_ball(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color ball_color = Red;
+  if (var_left || var_right) {
+    setPixel(row, column, ball_color);
+    setPixel(row, column - 1, ball_color);
+    setPixel(row, column + 1, ball_color);
+  } else if (var_up || var_down) {
+    setPixel(row, column, ball_color);
+    setPixel(row - 1, column, ball_color);
+    setPixel(row + 1, column, ball_color);
+  }
+}
+void set_line2_ball(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color ball_color = Red;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 1 : row - 1, column + 2, ball_color);
+    setPixel(var_left ? row + 1 : row - 1, column + 1, ball_color);
+    setPixel(var_left ? row + 1 : row - 1, column + 0, ball_color);
+    setPixel(var_left ? row + 1 : row - 1, column - 1, ball_color);
+    setPixel(var_left ? row + 1 : row - 1, column - 2, ball_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 2, var_down ? column - 1 : column + 1, ball_color);
+    setPixel(row + 1, var_down ? column - 1 : column + 1, ball_color);
+    setPixel(row + 0, var_down ? column - 1 : column + 1, ball_color);
+    setPixel(row - 1, var_down ? column - 1 : column + 1, ball_color);
+    setPixel(row - 2, var_down ? column - 1 : column + 1, ball_color);
+  }
+}
+void set_line3_ball(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color ball_color = Red;
+  if (var_left || var_right) {
+    // setPixel(var_left ? row + 2 : row - 2, column + 3, ball_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 2, ball_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 1, ball_color);
+    setPixel(var_left ? row + 2 : row - 2, column + 0, ball_color);
+    setPixel(var_left ? row + 2 : row - 2, column - 1, ball_color);
+    setPixel(var_left ? row + 2 : row - 2, column - 2, ball_color);
+    // setPixel(var_left ? row + 2 : row - 2, column - 3, ball_color);
+  } else if (var_up || var_down) {
+    // setPixel(row + 3, var_down ? column - 2 : column + 2, ball_color);
+    setPixel(row + 2, var_down ? column - 2 : column + 2, ball_color);
+    setPixel(row + 1, var_down ? column - 2 : column + 2, ball_color);
+    setPixel(row + 0, var_down ? column - 2 : column + 2, ball_color);
+    setPixel(row - 1, var_down ? column - 2 : column + 2, ball_color);
+    setPixel(row - 2, var_down ? column - 2 : column + 2, ball_color);
+    //   setPixel(row - 3, var_down ? column - 2 : column + 2, ball_color);
+  }
+}
+void set_line4_ball(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color ball_color = Red;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 3 : row - 3, column - 2, ball_color);
+    setPixel(var_left ? row + 3 : row - 3, column - 1, ball_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 1, ball_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 2, ball_color);
+    setPixel(var_left ? row + 3 : row - 3, column + 0, ball_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 0, var_down ? column - 3 : column + 3, ball_color);
+    setPixel(row + 2, var_down ? column - 3 : column + 3, ball_color);
+    setPixel(row + 1, var_down ? column - 3 : column + 3, ball_color);
+    setPixel(row - 1, var_down ? column - 3 : column + 3, ball_color);
+    setPixel(row - 2, var_down ? column - 3 : column + 3, ball_color);
+  }
+}
+void set_line5_ball(int row, int column, bool var_up, bool var_down, bool var_left, bool var_right) {
+  Color ball_color = Red;
+  if (var_left || var_right) {
+    setPixel(var_left ? row + 4 : row - 4, column + 0, ball_color);
+    setPixel(var_left ? row + 4 : row - 4, column + 1, ball_color);
+    setPixel(var_left ? row + 4 : row - 4, column - 1, ball_color);
+  } else if (var_up || var_down) {
+    setPixel(row + 1, var_down ? column - 4 : column + 4, ball_color);
+    setPixel(row + 0, var_down ? column - 4 : column + 4, ball_color);
+    setPixel(row - 1, var_down ? column - 4 : column + 4, ball_color);
+  }
+}
+
+/**************Setting Lines for Ball - Ends*****************/
+
+void drawBall(int row, int column, direction_e direction) {
+  fillFrameBuffer_Inside_Grid_Black();
+  bool var_up = 0, var_down = 0, var_left = 0, var_right = 0;
+
+  if (direction == LEFT_UP || direction == LEFT_DOWN) {
+    var_left = 1;
+  }
+  if (direction == UP_RIGHT || direction == UP_LEFT) {
+    var_up = 1;
+  }
+  if (direction == RIGHT_DOWN || direction == RIGHT_UP) {
+    var_right = 1;
+  }
+  if (direction == DOWN_LEFT || direction == DOWN_RIGHT) {
+    var_down = 1;
+  }
+  set_line1_ball(row, column, var_up, var_down, var_left, var_right);
+  set_line2_ball(row, column, var_up, var_down, var_left, var_right);
+  set_line3_ball(row, column, var_up, var_down, var_left, var_right);
+  set_line4_ball(row, column, var_up, var_down, var_left, var_right);
+  set_line5_ball(row, column, var_up, var_down, var_left, var_right);
+}
+
+void drawPaddle1(int row, int column, direction_e direction) {
+  fillFrameBuffer_Inside_Grid_Black();
+  Color paddle1_color = Blue;
+  if (direction == LEFT_DOWN) {
+    for (int i = 0; i < 16; i++) {
+      setPixel(row + i, column, paddle1_color);
+      setPixel(row + i, column + 1, paddle1_color);
+    }
+  }
+  if (direction == RIGHT_DOWN) {
+    for (int i = 0; i < 16; i++) {
+      setPixel(row - i, column, paddle1_color);
+      setPixel(row - i, column + 1, paddle1_color);
+    }
+  }
+}
+void drawPaddle2(int row, int column, direction_e direction) {
+  fillFrameBuffer_Inside_Grid_Black();
+  Color paddle2_color = Green;
+  if (direction == LEFT_UP) {
+    for (int i = 0; i < 16; i++) {
+      setPixel(row + i, column, paddle2_color);
+      setPixel(row + i, column - 1, paddle2_color);
+    }
+  }
+  if (direction == RIGHT_UP) {
+    for (int i = 0; i < 16; i++) {
+      setPixel(row - i, column, paddle2_color);
+      setPixel(row - i, column - 1, paddle2_color);
+    }
+  }
+}
